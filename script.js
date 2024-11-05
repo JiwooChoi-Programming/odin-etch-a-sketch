d23efe58f42070c6ce32a76b3edf4569e6143e0b
Jiwoo Choi <jiwooch13@gmail.com>
2024-11-05 17:35:30 +0100

    Update createGrid function and fix repeat style brackets

--- script.js
+++ script.js
@@ -7,9 +7,32 @@ buttonContainer.addEventListener("click", () => {
 
 function restartGrid() {
     let number = prompt("How would you like the grid size (1 - 100)", "100");
-    gridContainer.style.gridTemplateRows = `repeat${number}, 1fr`;
-    gridContainer.style.gridTemplateColumns = `repeat${number}, 1fr`;
+    gridContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
     createGrid(number);
 }
 
-restartGrid();
+function createGrid(size) {
+    gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        let square = document.createElement("div");
+        square.style.backgroundColor = "white";
+        gridContainer.appendChild(square);
+
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        });
+
+        function clearGrid() {
+            buttonContainer.addEventListener("click", () => {
+                square.style.backgroundColor = "white";
+            })
+        }
+
+        clearGrid();
+    }
+}
+
+createGrid(16);
