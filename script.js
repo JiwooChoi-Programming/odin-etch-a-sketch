bc498a829f16d609404402a2013d138f59cab812
Jiwoo Choi <jiwooch13@gmail.com>
2024-11-10 09:54:26 +0100

    Create the colorDiv, setColor, resetBoard functions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", () => {
     createBoard(16);
 
@@ -19,9 +21,7 @@ function createBoard(size) {
 
     for(let i = 0; i < numDivs; i++) {
         let div = document.createElement("div");
-        div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = "black";
-        })
+        div.addEventListener("mouseover", colorDiv);
         board.appendChild(div);
     }
 }
@@ -38,4 +38,23 @@ function getSize() {
         message.textContent = "Now you draw!"
         return input;
     }
+}
+
+function colorDiv() {
+    if (color === "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = "black";
+    }
+}
+
+function setColor(colorChoice) {
+    color = colorChoice;
+}
+
+function resetBoard() {
+    let divs = document.querySelectorAll("div");
+    divs.forEach((div) => {
+        div.style.backgroundColor = "white";
+    })
 }
